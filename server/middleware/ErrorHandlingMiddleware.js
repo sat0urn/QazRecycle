b28142ee3463e7b0c3f69f7b9a77b1dd34ddd61a
sat0urn <nissanfordo03@gmail.com>
2022-05-26 18:05:37 +0600

    some changes

--- server/middleware/ErrorHandlingMiddleware.js
+++
@@ -1,9 +0,0 @@
-const ApiError = require('../error/ApiError')
-const e = require("express");
-
-module.exports = function (err, req, res, next) {
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
-    }
-    return res.status(500).json({message: "Непредвиденная ошибка!"})
-}
