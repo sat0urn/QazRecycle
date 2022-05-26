1bec3b87e64ad1f0cf973cd38ca60fb5bdf352fe
sat0urn <nissanfordo03@gmail.com>
2022-05-26 15:54:03 +0600

    new

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.static("public"))
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
